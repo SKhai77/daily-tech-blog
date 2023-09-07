0198bca947e87d0f3795296409c6c776ac744a25
SKhai77 <orionkhai@gmail.com>
2023-09-07 15:19:11 -0500

    Created post route to create comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -9,7 +9,21 @@ router.get('/', async (req, res) => {
   } catch(err) {
     res.status(400).json(err);
   }
-})
+});
 
+// CREATE POST ROUTE TO CREATE COMMENT
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id, // Set the user_id of the comment to the logged-in user's id
+    });
 
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Export the router
 module.exports = router;
