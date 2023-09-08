ccbbd779dc18447075204e29d63fd369e6b4868d
SKhai77 <orionkhai@gmail.com>
2023-09-08 15:38:27 -0500

    Add comments to the code

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,4 @@
+// Function to handle user login form submission
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
@@ -22,32 +23,40 @@ const loginFormHandler = async (event) => {
   }
 };
 
+// Function to handle user sign-up form submission
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
+  // Get user input from the sign-up form
+  const username = document.querySelector('#name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (name && email && password) {
+  // Check if all required fields are filled
+  if (username && email && password) {
+    // Send a POST request to the '/api/users' endpoint with user data
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ username, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
+    // If the response is successful, redirect to the user's profile page
     if (response.ok) {
       document.location.replace('/profile');
     } else {
+      // Display an alert with the error message if the response is not ok
       alert(response.statusText);
     }
   }
 };
 
+// Event listener for the login form submission
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
 
+// Event listener for the sign-up form submission
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
