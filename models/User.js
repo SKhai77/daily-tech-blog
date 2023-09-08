c42b69fa50f79915650935a5e00c88a325410165
SKhai77 <orionkhai@gmail.com>
2023-09-08 15:36:38 -0500

    Add commens to the code

--- models/User.js
+++ models/User.js
@@ -1,13 +1,17 @@
+// Import necessary modules
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+// Define the User model class that extends Sequelize's Model class
 class User extends Model {
+  // Method to check if a provided password matches the stored password
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
+// Initialize the User model with its fields and configurations
 User.init(
   {
     id: {
@@ -16,33 +20,36 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true, // Ensure that usernames are unique
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
+      unique: true, // Ensure that email addresses are unique
       validate: {
-        isEmail: true,
+        isEmail: true, // Validate that the email field follows the email format
       },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8],
+        len: [8], // Validate that passwords are at least 8 characters long
       },
     },
   },
   {
     hooks: {
       beforeCreate: async (newUserData) => {
+        // Hash the password before creating a new user
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
       beforeUpdate: async (updatedUserData) => {
+        // Hash the password before updating the user's password
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
       },
@@ -55,4 +62,5 @@ User.init(
   }
 );
 
+// Export the User model
 module.exports = User;
