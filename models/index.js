62e16442ead9af2c50eb2724947a93adadc11d52
SKhai77 <orionkhai@gmail.com>
2023-09-08 15:30:01 -0500

    Add comments to the code and Cascading deletions

--- models/index.js
+++ models/index.js
@@ -1,18 +1,23 @@
+// Import the User, Post, and Comment models
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require("./Comment")
 
+// Define associations between User, Post, and Comment models
 User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(Post, {
-  foreignKey: 'post_id'
-})
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
 
+// Export the User, Post, and Comment models
 module.exports = { User, Post, Comment };
