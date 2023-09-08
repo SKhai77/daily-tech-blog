73b98e6233216fc69f7f052013b5b37bbf9dcf25
SKhai77 <orionkhai@gmail.com>
2023-09-08 15:15:42 -0500

    Add comments to the code and create post route for comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,26 +1,38 @@
+// Import necessary modules and initialize the Express.js router
 const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// Define a route that handles GET requests to retrieve comments
 router.get('/', async (req, res) => {
   try {
-    const data = await Comment.findAll()
-    res.status(400).json(data)
-  } catch(err) {
-    res.status(400).json(err);
+    const data = await Comment.findAll();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
-// CREATE POST ROUTE TO CREATE COMMENT
+// Create post route to create comment
 router.post('/', withAuth, async (req, res) => {
   try {
+    const { commentBody, post_id } = req.body;
+    const { username } = req.session; // Get the username from the session
+
+    if (!username) {
+      // Handle the case where username is not available (e.g., user is not authenticated)
+      return res.status(401).json({ error: 'User is not authenticated.' });
+    }
+
     const newComment = await Comment.create({
-      ...req.body,
-      user_id: req.session.user_id, // Set the user_id of the comment to the logged-in user's id
+      commentBody,
+      username, // Attach the username to the comment
+      post_id,
     });
 
-    res.status(200).json(newComment);
+    res.status(201).json(newComment);
   } catch (err) {
+    console.error(err);
     res.status(400).json(err);
   }
 });
