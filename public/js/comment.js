0fd33d62219686ccff83b378043d305ab2b1ce65
SKhai77 <orionkhai@gmail.com>
2023-09-10 21:22:17 -0500

    Add code for querySelector, comment button and data post id and fetch the user id from the html elemnt

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,11 +2,21 @@
 document.getElementById('comment-form').addEventListener('submit', async (e) => {
   e.preventDefault();
 
+  // Get the comment input and post_id from the button's data attribute
   const commentBody = document.querySelector('.commentInput').value;
-  const post_id = req.params.id;
-  const user_id = req.session.user_id;
+  const post_id = document.querySelector('#comment-button').getAttribute('data-post-id');
 
   try {
+    // Fetch the user_id from the HTML element
+    const user_idElement = document.querySelector('#user-id');
+    const user_id = user_idElement ? user_idElement.getAttribute('data-user-id') : null;
+
+    if (!user_id) {
+      // If user_id is not available, handle the error gracefully
+      console.error('User ID not found.');
+      return;
+    }
+
     const response = await fetch('/api/comments', {
       method: 'POST',
       body: JSON.stringify({ commentBody, post_id, user_id }), // Include user_id
@@ -14,7 +24,7 @@ document.getElementById('comment-form').addEventListener('submit', async (e) => 
     });
 
     if (response.ok) {
-      // Reload the page or update the comment section
+      // Comment successfully created, refresh the page or update the comments section
       location.reload();
     } else {
       // Handle error
